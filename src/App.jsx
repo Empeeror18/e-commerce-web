c52a82cdc5a25819344f98412ef1a7bdf126a299
Empeeror18 <saryal878@gmail.com>
2026-08-28 12:02:19 +0545

    add: routes and link

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import Auth from "../pages/Auth.jsx";
+import Checkout from "../pages/Checkout.jsx";
+import Navbar from "../components/Navbar.jsx";
+import { Routes, Route, Links } from "react-router-dom";
 
 function App() {
- 
+  return (
+    <div classname="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Auth" element={<Auth />} />
+        <Route path="/Checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
