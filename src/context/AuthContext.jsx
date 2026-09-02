de6b6e9b31100fc997ee7aa91b2b32913659f9b2
Empeeror18 <saryal878@gmail.com>
2026-09-02 10:35:01 +0545

    auth context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,20 +1,53 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { set } from "react-hook-form";
 
 export const AuthContext = createContext(null);
 
-export default function AuthProvider(children) {
-  const [user, setUser] = useState(null);
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+      ? { email: localStorage.getItem("currentUserEmail") }
+      : null,
+  );
 
-  function signup(email, password) {
-    const users = [];
+  function signUp(email, password) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
+    if (users.find((u) => u.email === email)) {
+      return { success: false, error: "Email already exists." };
+    }
     const newUser = { email, password };
     users.push(newUser);
-    localStorage.setItem("users", JSON.stringify({ users }));
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email);
+
+    setUser({ email });
+
+    return { success: true };
   }
 
-  function login() {}
+  function login(email, password) {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const user =
+      users.find((u) => u.email === email && u.password === password);
+
+    if (!user) {
+      return { success: false, error: "Invalid email or password" };
+    }
+
+    localStorage.setItem("currentUserEmail", email);
+    setUser({ email });
+
+    return { success: true };
+  }
+
+  function logout() {
+    localStorage.removeItem("currentUserEmail");
+    setUser(null);
+  }
   return (
-    <AuthContext.Provider value={{ signup }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signUp, user ,login,logout }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,27 +1,67 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 function Auth() {
   const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+  const { signUp, user, logout, login } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    setError(null);
+    let result;
+    if (mode == "signup") {
+      result = signUp(data.email, data.password);
+    } else {
+      result = login(data.email, data.password);
+    }
+
+    if (result.success) {
+      navigate("/");
+    } else {
+      setError(result.error);
+    }
+  }
+
   return (
     <div className="page">
       <div className="container">
         <div className="auth-container">
+          {user && <p>User logged in.</p>}
           <h1 className="auth-title">
             {mode === "signup" ? "Sign Up" : "Login"}
           </h1>
-          <form className="auth-form">
+          <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+            {error && <div className="error-msg">{error}</div>}
             <div className="form-group">
               <label className="form-label" htmlFor="email">
                 Email
               </label>
-              <input className="form-input" type="email" id="email" />
+              <input
+                className="form-input"
+                type="email"
+                id="email"
+                {...register("email")}
+              />
             </div>
             <div className="form-group">
               <label className="form-label" htmlFor="password">
                 Password
               </label>
-              <input className="form-input" type="password" id="password" />
+              <input
+                className="form-input"
+                type="password"
+                id="password"
+                {...register("password")}
+              />
             </div>
 
             <button type="submit" className="btn-primary">
